af31916e0b9e8f80d1856773df81d33888657bf6
Vlad Oreshko <orex3896@gmail.com>
2022-12-01 21:22:34 +0300

    Create task #1

---
+++ DZ1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if (A>max) max = A;
+if (B>max) max = B;
+
+Console.WriteLine(max); 
