d6f00d1df57851b180e07ce469fc537b4d0e423b
Vlad Oreshko <orex3896@gmail.com>
2022-12-01 21:32:28 +0300

    Create task #2

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите число А ");
+﻿/*
+Console.Write("Введите число А ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B ");
 int B = Convert.ToInt32(Console.ReadLine());
@@ -7,4 +8,22 @@ int max = A;
 if (A>max) max = A;
 if (B>max) max = B;
 
-Console.WriteLine(max); 
+Console.WriteLine("Максимальное число = " + max); 
+*/
+
+/*
+Console.Write("Введите число А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if (A>max) max = A;
+if (B>max) max = B;
+if (C>max) max = C;
+
+Console.WriteLine("Максимальное число = " + max);
+
+*/
